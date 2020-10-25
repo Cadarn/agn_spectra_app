a58886e6d17bfa84bc45b48d92bddb6d952358ca
Adam Hill <adam.ben.hill@gmail.com>
2020-10-25 19:17:02 +0000

    refactor: tidied up some of the layout

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
-import streamlit as st
+"""
+Streamlit app to host AGN spectral model generation
+Copyright: Adam Hill (2020)
+"""
+
 import plotly.express as px
+import streamlit as st
+
 from data_functions import generate_spectra
 
-degree_symbol = u"\N{DEGREE SIGN}"
+DEGREE_SYMBOL = "\N{DEGREE SIGN}"
 
 st.sidebar.title("AGN Spectral Model Simulator")
 
@@ -11,26 +17,32 @@ st.sidebar.subheader("AGN Geometry")
 st.sidebar.image("assets/schematic.png", use_column_width=True)
 
 # Controllers
-inclination = st.sidebar.slider("Inclination angle",
-                                min_value=0.,
-                                max_value=90.,
-                                step=0.1,
-                                format=f'%.1f{degree_symbol}',
-                                key="incAngle")
+inclination = st.sidebar.slider(
+    "Inclination angle",
+    min_value=0.0,
+    max_value=90.0,
+    step=0.1,
+    format=f"%.1f{DEGREE_SYMBOL}",
+    key="incAngle",
+)
 
-opening_angle = st.sidebar.slider("Torus opening angle",
-                                  min_value=0.,
-                                  max_value=90.,
-                                  step=0.1,
-                                  format=f'%.1f{degree_symbol}',
-                                  key="openingAngle")
+opening_angle = st.sidebar.slider(
+    "Torus opening angle",
+    min_value=0.0,
+    max_value=90.0,
+    step=0.1,
+    format=f"%.1f{DEGREE_SYMBOL}",
+    key="openingAngle",
+)
 
-column_density = st.sidebar.slider("Torus column density, logNH",
-                                   min_value=22.,
-                                   max_value=25.5,
-                                   step=0.1,
-                                   format=f'%.1f',
-                                   key="columnDensity")
+column_density = st.sidebar.slider(
+    "Torus column density, logNH",
+    min_value=22.0,
+    max_value=25.5,
+    step=0.1,
+    format="%.1f",
+    key="columnDensity",
+)
 
 # Add some context in the main window
 st.title("AGN Spectral Model Results")
@@ -38,20 +50,23 @@ st.subheader("X-ray spectrum")
 
 # generate our data
 df = generate_spectra(inclination, opening_angle, column_density)
-mod_df = df.melt(id_vars=['Energy'], value_vars=['Transmitted', "Reprocessed", "Summed"])
+mod_df = df.melt(
+    id_vars=["Energy"], value_vars=["Transmitted", "Reprocessed", "Summed"]
+)
 mod_df = mod_df.rename(columns={"variable": "Model Component", "value": "Flux"})
 
 # Construct our plot
-fig = px.line(mod_df, x="Energy", y="Flux", color='Model Component',
-              log_x=True, width=900, height=500,
-              labels=dict(Flux='EF_E / keV cm^{-2}s^{-1}', Energy="Energy / keV")
-              )
-fig.update_layout(legend=dict(
-    yanchor="top",
-    y=0.99,
-    xanchor="left",
-    x=0.01
-))
+fig = px.line(
+    mod_df,
+    x="Energy",
+    y="Flux",
+    color="Model Component",
+    log_x=True,
+    width=900,
+    height=500,
+    labels=dict(Flux="EF_E / keV cm^{-2}s^{-1}", Energy="Energy / keV"),
+)
+fig.update_layout(legend=dict(yanchor="top", y=0.99, xanchor="left", x=0.01))
 
 st.plotly_chart(fig)
 
--- data_functions.py
+++ data_functions.py
@@ -1,18 +1,42 @@
+"""
+AGN spectral model generation functions
+Copyright: Adam Hill (2020)
+"""
+
 import numpy as np
 import pandas as pd
 
+
 def generate_spectra(angle1, angle2, logNH):
-    """"This is a place holder for a proper function"""
-    _degs_to_rads = lambda x: np.pi*x/180.
+    """
+    This is a place holder for a proper function
+    Args:
+        angle1: float, inclination angle in degrees (0-90) of the AGN view
+        angle2: float, torus opening angle in degrees (0-90) of the AGN
+        logNH: float, logarithm of the obscuring column density within the AGN environment
+
+    Returns:
+        dataframe: a dataframe of with columns for the energy in keV, the transmitted X-ray flux,
+                   the reprocessed X-ray flux, and the total X-ray flux
+    """
+    _degs_to_rads = lambda x: np.pi * x / 180.0
     degrees = np.arange(1, 1001, 1)
     radians = np.array(list(map(_degs_to_rads, degrees)))
-    linear = radians*(logNH/9.657) + 2
-    ts1 = (angle1/5) * np.cos(_degs_to_rads(angle1) + radians*(logNH/1.5)) + linear
-    ts2 = (angle2/10) * np.sin(_degs_to_rads(angle2) + radians*(logNH/5.)) + 5.
-    combined = ts1 + ts2
+    linear_component = radians * (logNH / 9.657) + 2
+    transmitted_flux = (angle1 / 5) * np.cos(
+        _degs_to_rads(angle1) + radians * (logNH / 1.5)
+    ) + linear_component
+    reprocessed_flux = (angle2 / 10) * np.sin(
+        _degs_to_rads(angle2) + radians * (logNH / 5.0)
+    ) + 5.0
+    total_flux = transmitted_flux + reprocessed_flux
 
-    df = pd.DataFrame({"Energy": degrees,
-                       "Transmitted": ts1,
-                       "Reprocessed": ts2,
-                       "Summed": combined})
-    return df
+    spectral_df = pd.DataFrame(
+        {
+            "Energy": degrees,
+            "Transmitted": transmitted_flux,
+            "Reprocessed": reprocessed_flux,
+            "Summed": total_flux,
+        }
+    )
+    return spectral_df
